d200ee17b8a58abb1cace3e1da4ee56104759aec
AresRebatto <aresrebatt@gmail.com>
2023-03-09 10:48:45 +0100

    Inclusione Community Maui Toolkit(da fixare)

--- Memory/MauiProgram.cs
+++ Memory/MauiProgram.cs
@@ -1,12 +1,18 @@
-﻿namespace Memory;
+﻿using CommunityToolkit.Maui;
+
+namespace Memory;
 
 public static class MauiProgram
 {
 	public static MauiApp CreateMauiApp()
 	{
 		var builder = MauiApp.CreateBuilder();
-		builder
+
+        builder.UseMauiApp<App>().UseMauiCommunityToolkit();
+
+        builder
 			.UseMauiApp<App>()
+			.UseMauiCommunityToolkit()
 			.ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
