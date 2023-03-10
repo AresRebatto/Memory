45875cf08b95434abd4c32d0eaa679474eeb8380
GBABB1806 <gabriele.abbruscato06@gmail.com>
2023-03-10 11:57:55 +0100

    Aggiornamento Picker

--- Memory/SinglePlayer.xaml.cs
+++ Memory/SinglePlayer.xaml.cs
@@ -6,7 +6,23 @@ public partial class SinglePlayer : ContentPage
 	{
 		InitializeComponent();
 		SceltaModalita.SelectedItem = "Facile";
-	}
+        SceltaModalita.SelectedIndexChanged += (sender, args) =>
+        {
+            // codice per gestire l'evento di selezione dell'item
+            if (SceltaModalita.SelectedIndex == 0)
+            {
+                //numero diCoppie
+            }
+            else if (SceltaModalita.SelectedIndex == 1)
+            {
+                // codice da eseguire se l'item "Option 2" è selezionato
+            }
+            else if (SceltaModalita.SelectedIndex == 2)
+            {
+                // codice da eseguire se l'item "Option 3" è selezionato
+            }
+        };
+    }
 
     private void Button_Clicked(object sender, EventArgs e)
     {
